b951b355797ab12ffd55042cb5395f4b52923172
James Ngandu <jamesmngandu@gmail.com>
2023-08-01 13:07:12 +0300

    added

---
+++ script.js
@@ -0,0 +1,5 @@
+let githubBtn = document.querySelector('.github-button');
+githubBtn.addEventListener("click", function(){
+    alert("For now search me at jmngandu")
+    alert("Am still constructing the portfolio")
+})
